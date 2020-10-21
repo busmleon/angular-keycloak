db7378cc8d49c4dda710ca9a7c9f6b9d6c983bf3
busmleon <leon.schoolmail@web.de>
2020-10-21 14:15:36 +0200

    busmleon.de

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  KEYCLOAK_URL: 'http://localhost:8080/auth',
+  KEYCLOAK_URL: 'https://admin.busmleon.de/auth',
   KEYCLOAK_REALM: 'APP_REALM',
   KEYCLOAK_CLIENT: 'FrontendClient',
-  BACKEND_URL: 'http://localhost:3000/db'
+  BACKEND_URL: 'https://backend.busmleon.de/db'
 };
