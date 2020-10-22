8ef2e19ac63d05d580f5b53733270534c2f0bab2
busmleon <leon.schoolmail@web.de>
2020-10-22 11:57:39 +0200

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,7 +17,22 @@ export class AppComponent {
 
   }
   onGetTestobjects(area: HTMLTextAreaElement): void {
-    this.httpClient.get<User[]>(baseURL + '/user', { headers: { 'Authorization': 'bearer ' + localStorage.getItem('ang-token') } }).pipe(
+    this.httpClient.get<User[]>(baseURL + '/user').pipe(
+      catchError((err) => {
+        if (err.status == 403)
+          alert('You don\'t have the permission to do that!')
+        return throwError(err);
+      })
+    ).subscribe(
+      res => {
+        var temp = '';
+        for (let user of res)
+          temp += JSON.stringify(user);
+        area.value = temp;
+      });
+  }
+  onGetTestobjects2(area: HTMLTextAreaElement): void {
+    this.httpClient.get<User[]>(baseURL + '/admin', { headers: { 'Authorization': 'bearer ' + localStorage.getItem('ang-token') } }).pipe(
       catchError((err) => {
         if (err.status == 403)
           alert('You don\'t have the permission to do that!')
